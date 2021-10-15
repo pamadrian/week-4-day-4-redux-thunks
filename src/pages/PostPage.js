73447193431df7f15509e2d00d590cc1d984c173
pamadrian <pamvanalfen@icloud.com>
2021-10-15 13:32:17 +0200

    used selector in PostPage component and displayed loading message when post is still loading and done when the post has been fetched

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -2,12 +2,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchPost } from "../store/postPage/actions";
+import { selectPostAndComments } from "../store/postPage/selectors";
 
 export default function PostPage() {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const loading = useSelector(selectPostAndComments);
   console.log(id);
 
   useEffect(() => {
@@ -18,7 +20,9 @@ export default function PostPage() {
     <div>
       {" "}
       <h1 align="center">Hello this is the postpage!</h1>
-      <div></div>
+      <div>
+        <p>{loading ? <em>Loading...</em> : <p> Done! </p>}</p>
+      </div>
     </div>
   );
 }
